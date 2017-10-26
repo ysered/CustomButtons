aac9d3c1a7f3470af4e727a450a9ad5cb9e9c81d
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2017-10-26 19:06:48 +0300

    Rename params.

--- app/src/main/java/com/ysered/custombuttons/utils/Math.kt
+++ app/src/main/java/com/ysered/custombuttons/utils/Math.kt
@@ -3,8 +3,8 @@ package com.ysered.custombuttons.utils
 /**
  * Checks whether [x] and [y] is inside circle.
  */
-fun isInsideCircle(x: Float, y: Float, circleX: Float, circleY: Float, circleRadius: Float): Boolean {
-    val absX = Math.pow(Math.abs(x - circleX).toDouble(), 2.0)
-    val absY = Math.pow(Math.abs(y - circleY).toDouble(), 2.0)
+fun isInsideCircle(x: Float, y: Float, circleCenterX: Float, circleCenterY: Float, circleRadius: Float): Boolean {
+    val absX = Math.pow(Math.abs(x - circleCenterX).toDouble(), 2.0)
+    val absY = Math.pow(Math.abs(y - circleCenterY).toDouble(), 2.0)
     return Math.sqrt(absX + absY) < circleRadius
 }
